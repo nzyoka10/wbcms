709e20ba90932ae3540196da43d41dc03f1ecd2a
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-06-29 18:18:53 +0300

    customer homepage

---
+++ customer.php
@@ -0,0 +1,125 @@
+<?php
+session_start();
+
+// Check if the user is logged in, if not redirect to the login page
+if (!isset($_SESSION['user_id'])) {
+    header("Location: index.php");
+    exit();
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <title>WBCM | Dashboard</title>
+
+  <!-- Montserrat Font -->
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+  <!-- Material Icons -->
+  <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+  <div class="grid-container">
+
+    <!-- Header -->
+    <header class="header">
+      <div class="menu-icon" onclick="openSidebar()">
+        <span class="material-icons-outlined">menu</span>
+      </div>
+      <div class="header-left">
+        <h4 class="text-primary">Water Billing & Customer Management - <?php echo htmlspecialchars($_SESSION['email']); ?></h4>
+      </div>
+      <div class="header-right text-primary">
+        <!-- Message Notification banner -->
+        <a href="#">
+          <span class="material-icons-outlined">notifications</span>
+        </a>
+        <a href="#">
+          <span class="material-icons-outlined">email</span>
+        </a>
+        <a href="#">
+          <span class="material-icons-outlined">account_circle</span>
+        </a>
+      </div>
+    </header>
+    <!-- End Header -->
+
+    <!-- Sidebar -->
+    <aside id="sidebar">
+      <div class="sidebar-title">
+        <div class="sidebar-brand">
+          <span class="material-icons-outlined">water_drop</span> AquaBill
+        </div>
+        <span class="material-icons-outlined" onclick="closeSidebar()">close</span>
+      </div>
+
+      <ul class="sidebar-list">
+        <li class="sidebar-list-item">
+          <a href="./dashboard.php">
+            <span class="material-icons-outlined">dashboard</span>&nbsp;&nbsp;Dashboard
+          </a>
+        </li>
+
+        <li class="sidebar-list-item">
+          <a href="./customer.php">
+            <span class="material-icons-outlined">group</span>&nbsp;&nbsp;Customer
+          </a>
+        </li>
+
+        <li class="sidebar-list-item">
+          <a href="#">
+            <span class="material-icons-outlined">paid</span>&nbsp;&nbsp;Billing
+          </a>
+        </li>
+        <li class="sidebar-list-item">
+          <a href="#">
+            <span class="material-icons-outlined">poll</span>&nbsp;&nbsp;Reports
+          </a>
+        </li>
+        <li class="sidebar-list-item">
+          <a href="#">
+            <span class="material-icons-outlined">settings</span>&nbsp;&nbsp;Settings
+          </a>
+        </li>
+        <li class="sidebar-list-item">
+          <a href="./logout.php">
+            <span class="material-icons-outlined">logout</span>&nbsp;&nbsp;Logout
+          </a>
+        </li>
+   
+      </ul>
+    </aside>
+    <!-- End Sidebar -->
+
+    <!-- Main -->
+    <main class="main-container">
+      <div class="main-title">
+        <p class="font-weight-bold">DASHBOARD</p>
+      </div>
+
+      
+
+
+
+    </main>
+    <!-- End Main -->
+
+  </div>
+
+  <!-- Scripts -->
+  <!-- ApexCharts -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/3.35.3/apexcharts.min.js"></script>
+  <!-- Custom JS -->
+  <script src="js/scripts.js"></script>
+</body>
+</html>
