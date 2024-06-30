2bdc75e114a5221e71cf0cec6126522e9ba04c1e
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-06-30 09:57:26 +0300

    customer data

--- customer.php
+++ customer.php
@@ -25,6 +25,10 @@ if (!isset($_SESSION['user_id'])) {
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
 
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="css/bootstrap5.0.1.min.css">
+  <link rel="stylesheet" type="text/css" href="css/datatables-1.10.25.min.css" />
+
   <!-- Custom CSS -->
   <link rel="stylesheet" href="css/styles.css">
   <link rel="stylesheet" href="css/customer.css">
@@ -38,7 +42,7 @@ if (!isset($_SESSION['user_id'])) {
         <span class="material-icons-outlined">menu</span>
       </div>
       <div class="header-left">
-        <h4 class="text-primary">Water Billing & Customer Management - <?php echo htmlspecialchars($_SESSION['email']); ?></h4>
+        <h4 class="text-secondary"><strong>WBCM</strong>&nbsp;&nbsp;-&nbsp;&nbsp;Clients</h4>
       </div>
       <div class="header-right text-primary">
         <!-- Message Notification banner -->
@@ -73,7 +77,7 @@ if (!isset($_SESSION['user_id'])) {
 
         <li class="sidebar-list-item">
           <a href="./customer.php">
-            <span class="material-icons-outlined">group</span>&nbsp;&nbsp;Customer
+            <span class="material-icons-outlined">group</span>&nbsp;&nbsp;List of Clients
           </a>
         </li>
 
@@ -104,43 +108,39 @@ if (!isset($_SESSION['user_id'])) {
 
     <!-- Main -->
     <main class="main-container">
-      <!-- <div class="main-title">
-        <p class="font-weight-bold">DASHBOARD</p>
-      </div> -->
-
-      <nav>
-        <ul>
-          <li><a href="" class="lnk">New Customer</a></li>
-          <li><a href="" class="lnk">View All</a></li>
-          <li><a href="" class="lnk">Meter A/C</a></li>
-        </ul>
-      </nav>
-
+   
+    <div class="row">
       <div class="container">
 
-        <!-- register new customer -->
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-            <div class="input-box">
-                <input type="text" name="email" class="input-field" placeholder="Email id" autocomplete="off" required>
-            </div>
-            <div class="input-box">
-                <input type="password" name="password" class="input-field" placeholder="Password" autocomplete="off" required>
-            </div>
-            <div class="forgot">
-                <section>
-                    <input type="checkbox" id="check">
-                    <label for="check">Remember me</label>
-                </section>
-                <section>
-                    <a href="#">Forgot password</a>
-                </section>
-            </div>
-            <div class="input-submit">
-                <button class="submit-btn" id="submit"></button>
-                <label for="submit">Sign In</label>
-            </div>
-        </form>
+        <!-- Add account modal -->
+        <div class="btnAdd">
+          <a href="#!" data-id="" data-bs-toggle="modal" data-bs-target="#addUserModal" class="btn btn-success btn-sm">New Client</a>
+        </div>
+
+        <!-- Data table -->
+        <div class="row">
+          <div class="col-md-2"></div>
+          <div class="col-md-8">
+            <table id="example" class="table">
+              <thead>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Mobile</th>
+                <th>Address</th>
+                <th>Meter ID</th>
+                <th>Status</th>
+                <th>Options</th>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+          </div>
+          <div class="col-md-2"></div>
+        </div>
+
       </div>
+    </div>
 
       
 
@@ -151,10 +151,130 @@ if (!isset($_SESSION['user_id'])) {
 
   </div>
 
-  <!-- Scripts -->
+
   <!-- ApexCharts -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/3.35.3/apexcharts.min.js"></script>
-  <!-- Custom JS -->
+  <script src="js/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
+  <script src="js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="js/dt-1.10.25datatables.min.js"></script>
   <script src="js/scripts.js"></script>
+  <script src="js/script.js"></script>
+
+
+  <!-- Add user Modal -->
+  <div class="modal fade" id="addUserModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add New Client</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form id="addUser" action="post">
+                    <div class="mb-3 row">
+                        <label for="addUserField" class="col-md-3 form-label">Full Name</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" id="addUserField" name="name" placeholder="Client Name">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="addEmailField" class="col-md-3 form-label">Email</label>
+                        <div class="col-md-9">
+                            <input type="email" class="form-control" id="addEmailField" name="email" placeholder="Email Address">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="addMobileField" class="col-md-3 form-label">Contact #</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" id="addMobileField" name="mobile" placeholder="Phone Number">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="addAddressField" class="col-md-3 form-label">Address</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" id="addAddressField" name="address" placeholder="Nairobi, Kenya">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="addMeterIdField" class="col-md-3 form-label">Meter ID</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" id="addMeterIdField" name="meter_id" placeholder="Meter Number">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="addMeterIdField" class="col-md-3 form-label">Initial Reading</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" id="addFirstReadingField" name="first_reading" placeholder="Meter Number">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="addStatusField" class="col-md-3 form-label">Status</label>
+                        <div class="col-md-9">
+                            <select name="status" id="addStatusField" class="form-control">
+                                <option value="inactive">Inactive</option>
+                                <option value="active">Active</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+  <!-- Modal to Update user -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Update User</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form id="updateUser">
+            <input type="hidden" name="id" id="id" value="">
+            <input type="hidden" name="trid" id="trid" value="">
+            <div class="mb-3 row">
+              <label for="nameField" class="col-md-3 form-label">Name</label>
+              <div class="col-md-9">
+                <input type="text" class="form-control" id="nameField" name="name">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="emailField" class="col-md-3 form-label">Email</label>
+              <div class="col-md-9">
+                <input type="email" class="form-control" id="emailField" name="email">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="mobileField" class="col-md-3 form-label">Mobile</label>
+              <div class="col-md-9">
+                <input type="text" class="form-control" id="mobileField" name="mobile">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="cityField" class="col-md-3 form-label">City</label>
+              <div class="col-md-9">
+                <input type="text" class="form-control" id="cityField" name="City">
+              </div>
+            </div>
+            <div class="text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </body>
 </html>
