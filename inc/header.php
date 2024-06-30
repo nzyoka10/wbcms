2d575d4caac4f976639e80afc1d51275636a2e35
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-06-30 15:38:00 +0300

    header

--- inc/header.php
+++ inc/header.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+include("config/functions.php");
+
+$totalUsers = countRegisteredUsers($conn);
+
 // Check if the user is logged in, if not redirect to the login page
 if (!isset($_SESSION['user_id'])) {
     header("Location: index.php");
